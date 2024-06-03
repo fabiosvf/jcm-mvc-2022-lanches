c5d478d7c525b935339fc4f477cc5ecd15f2c628
Fabio Silva Ferreira <fabiosvf@gmail.com>
2024-06-02 23:40:58 -0300

    Seção 4: NET 6 - Controllers e Views 57. Ajustando o LancheController para exibir lanches por categoria

--- LanchesMac/Controllers/LancheController.cs
+++ LanchesMac/Controllers/LancheController.cs
@@ -1,5 +1,6 @@
 ﻿using LanchesMac.Repositories.Interfaces;
 using LanchesMac.ViewModels;
+using LanchesMac.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LanchesMac.Controllers;
@@ -12,13 +13,39 @@ public class LancheController : Controller
         _lancheRepository = lancheRepository;
     }
 
-    public IActionResult List()
+    public IActionResult List(string categoria)
     {
-        //var lanches = _lancheRepository.Lanches;
-        //return View(lanches);
-        var lanchesListViewModel = new LancheListViewModel();
-		lanchesListViewModel.Lanches = _lancheRepository.Lanches;
-        lanchesListViewModel.CategoriaAtual = "Categoria Atual";
+        IEnumerable<Lanche> lanches;
+        string categoriaAtual = string.Empty;
+
+        if (string.IsNullOrEmpty(categoria))
+        {
+            lanches = _lancheRepository.Lanches.OrderBy(l => l.LancheId);
+            categoriaAtual = "Todos os lanches";
+        }
+        else
+        {
+            if (string.Equals("Normal", categoria, StringComparison.OrdinalIgnoreCase))
+            {
+                lanches = _lancheRepository.Lanches
+                    .Where(l => l.Categoria.CategoriaNome.Equals("Normal"))
+                    .OrderBy(l => l.Nome);
+            }
+            else
+            {
+                lanches = _lancheRepository.Lanches
+                    .Where(l => l.Categoria.CategoriaNome.Equals("Natural"))
+                    .OrderBy(l => l.Nome);
+            }
+            categoriaAtual = categoria;
+        }
+
+        var lanchesListViewModel = new LancheListViewModel()
+        {
+            Lanches = lanches,
+            CategoriaAtual=categoriaAtual
+        };
+		
         return View(lanchesListViewModel);
 	}
 }
