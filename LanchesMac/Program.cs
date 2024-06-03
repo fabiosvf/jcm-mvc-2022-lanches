7191619bbd7a2377e3dbdebd367079de0c273962
Fabio Silva Ferreira <fabiosvf@gmail.com>
2024-06-02 23:55:45 -0300

    Seção 4: NET 6 - Controllers e Views 59. Múltiplas Rotas - Criando a rota para exibir lanches por categoria

--- LanchesMac/Program.cs
+++ LanchesMac/Program.cs
@@ -39,8 +39,12 @@ app.UseSession();
 app.UseAuthorization();
 
 app.MapControllerRoute(
+    name: "categoriaFiltro",
+    pattern: "Lanche/{action}/{categoria?}",
+    defaults: new { Controller = "Lanche", Action = "List" });
+
+app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
-
 
 app.Run();
